cc3d682e8f7d194c75cb4c059a817eee2b2bed15
hackedlings <pks9862728888@gmail.com>
2023-03-29 21:13:04 +0530

    - Initial commit

---
+++ src/test/java/com/demo/exebuilderandmultistagebuild/ExeBuilderAndMultiStageBuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.exebuilderandmultistagebuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExeBuilderAndMultiStageBuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
